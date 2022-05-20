d739ef022b355a03e64f4c4051af35338ccfbd74
irusha <irushaathukorala@gmail.com>
2022-05-20 23:03:16 +0530

    Optimized page scrolling. Side bar updated

--- static/size_adjust.js
+++ static/size_adjust.js
@@ -69,6 +69,20 @@ function w3_open() {
   }
 
 }
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
 
 function scroll_to_folders() {
   let e = document.getElementById("folders");
