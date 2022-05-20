5af531cdea8fd3c9170a0d160a6af09cafecbea5
irusha <irushaathukorala@gmail.com>
2022-05-20 10:17:42 +0530

    Sidebar added

--- static/size_adjust.js
+++ static/size_adjust.js
@@ -1,11 +1,25 @@
-var video = this.document.getElementById('video_m')
+let video = this.document.getElementById('video_m')
 window.onload = function () {
-    video.setAttribute('width', document.body.clientWidth - 20)
+  video.setAttribute('width', document.body.clientWidth - 20)
 }
-window.addEventListener("resize", function(event) {
-    console.log(document.body.clientWidth + ' wide by ' + document.body.clientHeight+' high');
-    
-    video.setAttribute('width', document.body.clientWidth - 20)
-    
-  })
+window.addEventListener("resize", function (event) {
+  console.log(document.body.clientWidth + ' wide by ' + document.body.clientHeight + ' high');
 
+  video.setAttribute('width', document.body.clientWidth - 20)
+
+})
+
+c = true
+
+function w3_open() {
+  if (c) {
+    document.getElementById("rightMenu").style.display = "block";
+
+    c = false;
+  }
+  else {
+    document.getElementById("rightMenu").style.display = "none";
+    c = true;
+  }
+
+}
